8705e712626f50cc2269de0a49047290865a04bb
Shamama Guliyeva <shamamaguliyeva04@gmail.com>
2023-10-09 01:09:12 +0400

    task fixed2

--- 09.10task1/09.10task/Program.cs
+++ 09.10task1/09.10task/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Student student1 = new Student("Shamama", "Guliyeva", "AB104", 31);
+        Student student1 = new Student("Shamama", "Guliyeva", "AB104", 95);
         student1.GetInfo();
         student1.CheckGraduation();
         student1.GetDiplomDegree();
